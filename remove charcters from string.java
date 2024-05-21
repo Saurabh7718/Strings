9206e4a147887f0039741829f04840edf3e197ee
saurabh chidrawar <153104134+Saurabh7718@users.noreply.github.com>
2024-05-21 19:43:24 +0530

    Create remove charcters from string.java

---
+++ remove charcters from string.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Original string with numbers
+        String input = "saurabh123";
+        
+        // Extract all numeric characters
+        String output = input.replaceAll("[^\\d]", "");
+        
+        // Print the resulting string
+        System.out.println("Original string: " + input);
+        System.out.println("String with only numbers: " + output);
+    }
+}
+//output 123
